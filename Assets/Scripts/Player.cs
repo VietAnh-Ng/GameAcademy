d477d30e69c6378330536e13b87c8b0c44fb0011
Viet Anh <vietanh.tic@gmail.com>
2025-05-12 21:00:47 +0700

    upload project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Collider2D cl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+
+        rb.velocity = new(3, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
